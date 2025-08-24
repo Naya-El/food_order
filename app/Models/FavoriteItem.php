f235fdb4d5b1da0afe31464592aa4fe157c5e78a
Naya-El <102901112+Naya-El@users.noreply.github.com>
2025-08-24 16:04:13 +0300

    Update FavoriteItem.php

--- app/Models/FavoriteItem.php
+++ app/Models/FavoriteItem.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class FavoriteItem extends Model
 {
     use HasFactory;
+     public function items()
+    {
+        return $this->belongsTo(StandardItem::class, 'item_id');
+
+    }
 }
