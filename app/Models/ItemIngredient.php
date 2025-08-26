b901d006003e6c9d06642e1567a7f0eeb844ec15
Naya-El <102901112+Naya-El@users.noreply.github.com>
2025-08-26 20:23:29 +0300

    Update ItemIngredient.php

--- app/Models/ItemIngredient.php
+++ app/Models/ItemIngredient.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class ItemIngredient extends Model
 {
     use HasFactory;
+
+    public function ingredient()
+    {
+        return $this->belongsTo(Ingredient::class, 'ingredient_id');
+    }
 }
