f9a3ee37aa54c8d4d7eafde7255357c9f366f87d
Naya-El <102901112+Naya-El@users.noreply.github.com>
2025-08-27 23:05:40 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -28,12 +28,13 @@ Route::middleware('auth:sanctum')->group(function () {
     });
 
 
-    // Cart
-    Route::post('add-cart', [App\Http\Controllers\CartController::class, 'addToCart']);
-    Route::post('update-qty/{id}', [App\Http\Controllers\CartController::class, 'updateItemQuantity']);
-    Route::delete('remove-item/{id}', [App\Http\Controllers\CartController::class, 'removeItem']);
-    Route::delete('clear-cart/{id}', [App\Http\Controllers\CartController::class, 'clearCart']);
-    Route::post('confirm-cart', [App\Http\Controllers\CartController::class, 'confirmCart']);
+      // Cart
+    Route::post('add-cart',[App\Http\Controllers\CartController::class,'addToCart']);
+    Route::post('update-qty/{id}',[App\Http\Controllers\CartController::class,'updateItemQuantity']);
+    Route::delete('remove-item/{id}',[App\Http\Controllers\CartController::class,'removeItem']);
+    Route::delete('clear-cart/{id}',[App\Http\Controllers\CartController::class,'clearCart']);
+    Route::post('confirm-cart',[App\Http\Controllers\CartController::class,'confirmCart']);
+
 
     // Favorite
     Route::post('save-favorite', [App\Http\Controllers\HomeController::class, 'saveFavorite']);
@@ -41,9 +42,15 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::delete('remove-favorite-item/{id}', [App\Http\Controllers\HomeController::class, 'removeFromFavorite']);
     Route::delete('clear-favorite', [App\Http\Controllers\HomeController::class, 'clearFavorite']);
 
+    // Category
+    Route::get('categories',[App\Http\Controllers\HomeController::class,'categories']);
+
+    // Filters
+    Route::get('item-filter',[App\Http\Controllers\HomeController::class,'itemFilter']);
+
     // Items
-    Route::get('items', [App\Http\Controllers\HomeController::class, 'standardItem']);
-    Route::get('item-details/{id}', [App\Http\Controllers\HomeController::class, 'itemDetails']);
+    Route::get('items',[App\Http\Controllers\HomeController::class,'standardItem']);
+    Route::get('item-details/{id}',[App\Http\Controllers\HomeController::class,'itemDetails']);
 
 
     Route::get('/logout', [\App\Http\Controllers\AuthController::class, 'logout']);
