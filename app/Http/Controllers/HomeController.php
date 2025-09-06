bf8a70442efd18a22819ace39fceda9cb4ee20dc
Naya-El <102901112+Naya-El@users.noreply.github.com>
2025-09-06 21:07:49 +0300

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -296,14 +296,20 @@ class HomeController extends Controller
             'orders' => $orders
         ));
     }
-
-    public function orderDetails($id)
+ public function orderDetails($id)
     {
         $orderData = Order::where('id', $id)->first();
         $orderItems = OrderItem::where('order_id', $id)->get();
+        $totalPrice = $orderItems->sum(function ($item) {
+            return $item->price * $item->qty;
+        });
+        $orderData->total_price = $totalPrice;
+
+
         return response()->json(array(
-            'orderData' => $orderData,
-            'orderItems' => $orderItems
+            'order' => $orderData,
+            'items' => $orderItems,
+            'total_price' => $totalPrice,
         ));
     }
-}
+ }
