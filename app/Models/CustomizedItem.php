39efbf0814dc8f72ab6c5d9053108cc8cef75b9d
Naya-El <102901112+Naya-El@users.noreply.github.com>
2025-09-07 16:50:53 +0300

    Update CustomizedItem.php

--- app/Models/CustomizedItem.php
+++ app/Models/CustomizedItem.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class CustomizedItem extends Model
 {
     use HasFactory;
+
+    public function orderItems()
+    {
+        return $this->morphMany(OrderItem::class, 'item', 'item_type', 'item_id');
+    }
 }
